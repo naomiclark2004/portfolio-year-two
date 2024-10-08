e6e6a7d69843b1208a447a9611a91bb47599cebe
Naomi <naomiclark2004@gmail.com>
2024-10-08 09:14:09 -0700

    changed env file

---
+++ pages/api/data.js
@@ -0,0 +1,230 @@
+let projects = [
+  {
+    name: "AZ Landmarks",
+    date: "8/23/21",
+    description: "CSS project on text properties by creating a website.",
+    link: "https://naomiclark2004.github.io/az-landmarks/",
+    src: "images/grandcanyon.png",
+  },
+  {
+    name: "Landing Page",
+    date: "9/1/21",
+    description:
+      "CSS landing page project using google fonts and a background image.",
+    link: "https://naomiclark2004.github.io/landing-page/",
+    src: "images/landing-page.png",
+  },
+
+  {
+    name: "Resume",
+    date: "9/1/21",
+    description: "Resume project using font text and font face properties.",
+    link: "https://naomiclark2004.github.io/resume/",
+    src: "images/resume.png",
+  },
+
+  {
+    name: "Career Tree",
+    date: "10/21/21",
+    description:
+      "Project to compare the three levels of careers and show their lifestyle.",
+    link: "https://naomiclark2004.github.io/career-tree/",
+    src: "images/career tree.png",
+  },
+
+  {
+    name: "Registration Form",
+    date: "11/1/21",
+    description:
+      "JS project using functions and built off of an earlier form project.",
+    link: "https://naomiclark2004.github.io/registration-form/",
+    src: "images/registration form.png",
+  },
+
+  {
+    name: "Calculator",
+    date: "11/5/21",
+    description:
+      "Calculator project using JS to create a functional calculator.",
+    link: "https://naomiclark2004.github.io/calculator/",
+    src: "images/calculator.png",
+  },
+
+  {
+    name: "Quiz",
+    date: "11/9/21",
+    description:
+      "Quiz project using JS to create a functional, graded, and time quiz.",
+    link: "https://naomiclark2004.github.io/quiz/",
+    src: "images/quiz.png",
+  },
+
+  {
+    name: "Tic-Tac-Toe",
+    date: "11/17/21",
+    description:
+      "Tic-Tac-Toe project using JS to create a fully functioning tic-tac-toe game.",
+    link: "https://naomiclark2004.github.io/Tic-Tac_Toe/",
+    src: "images/tictactoe.png",
+  },
+
+  {
+    name: "CSS Transition",
+    date: "11/23/21",
+    description:
+      "CSS Transition project using transition properties to create transitions.",
+    link: "https://naomiclark2004.github.io/stylish-girl/",
+    src: "images/transition.png",
+  },
+
+  {
+    name: "Button Hover",
+    date: "11/30/21",
+    description:
+      "Button Hover project using transitions to change element properties on hover.",
+    link: "https://naomiclark2004.github.io/button-hover/",
+    src: "images/button.png",
+  },
+
+  {
+    name: "Animation",
+    date: "12/3/21",
+    description: "Animation project using CSS & JS to make an animation.",
+    link: "https://naomiclark2004.github.io/boy-animations/",
+    src: "images/animation.png",
+  },
+
+  {
+    name: "Hosting Plan",
+    date: "1/20/22",
+    description:
+      "Hosting Plan project using a JS constructer to make dynamic pricing.",
+    link: "https://naomiclark2004.github.io/hosting-plans/",
+    src: "images/hosting-plan.png",
+  },
+
+  {
+    name: "SVG and Canvas",
+    date: "2/3/22",
+    description:
+      "SVG & Canvas project using CSS & JS to create Svg and Canvas images.",
+    link: "https://naomiclark2004.github.io/svg-vs-canvas/",
+    src: "images/svg-canvas.png",
+  },
+
+  {
+    name: "JQuery Portfolio",
+    date: "2/10/22",
+    description:
+      "Portfolio project using jQuery library to create a portfolio template.",
+    link: "https://naomiclark2004.github.io/jquery-portfolio/",
+    src: "images/portfolio.png",
+  },
+
+  {
+    name: "Bootstrap Ecommerce",
+    date: "2/15/22",
+    description:
+      "Ecommerce project using Bootstrap & JS to create a dynamic ecommerce website.",
+    link: "https://naomiclark2004.github.io/ecommerce/",
+    src: "images/ecommerce.png",
+  },
+
+  {
+    name: "Digital Clock",
+    date: "3/11/22",
+    description:
+      "Digital Clock project using JS to create dynamic digital clock.",
+    link: "https://naomiclark2004.github.io/digital-clock/",
+    src: "images/digital-clock.png",
+  },
+
+  {
+    name: "JSON Places & Maps",
+    date: "3/28/22",
+    description:
+      "Places & Maps project using JS & JSON to dynamically embed maps in webpage.",
+    link: "https://naomiclark2004.github.io/dynamic-list/",
+    src: "images/places-maps.png",
+  },
+
+  {
+    name: "HTML, CSS, & JS Quiz",
+    date: "4/15/22",
+    description:
+      "Quiz using JS and JSON to dynamically create questions and grade quiz.",
+    link: "https://naomiclark2004.github.io/mta-quiz/", // only load after refresh
+    src: "images/capstone.png",
+  },
+  {
+    name: "Calendar",
+    date: "4/18/22",
+    description: "Calendar project using JS to create dynamic calendar widget.",
+    link: "https://naomiclark2004.github.io/calendar/",
+    src: "images/calendar.png",
+  },
+  // {
+  //   name: "Employee Database System",
+  //   date: "8/26/22",
+  //   description:
+  //     "Node and Express App using Mongodb to create Employee Registration System",
+  //   link: "https://employee-registration-system.onrender.com/", // slow loading
+  //   src: "images/employee.png",
+  // },
+  // {
+  //   name: "Music Artist Search App",
+  //   date: "10/26/22",
+  //   description:
+  //     "Music Artist Search project using Songsterr API. Created in React.js.",
+  //   link: "https://naomi-artist-search.netlify.app/",
+  //   src: "images/music.png",
+  // },
+  {
+    name: "Jokes App",
+    date: "20/31/22",
+    description:
+      "Jokes project using Norris Jokes API. Made in Next.js with MongoDB database.",
+    link: "https://naomi-norris-jokes.netlify.app/",
+    src: "images/laugh.png",
+  },
+  {
+    name: "Quotes Generator",
+    date: "11/1/22",
+    description: "React project using API to fetch random quotes.",
+    link: "https://naomi-quotes-generator.netlify.app/",
+    src: "images/quote.png",
+  },
+  {
+    name: "Movies App",
+    date: "11/3/22",
+    description: "Next.js project using movies API to create Movie Search App.",
+    link: "https://moviepage123.netlify.app/",
+    src: "images/movie.png",
+  },
+  {
+    name: "Weather App",
+    date: "11/7/22",
+    description: "React project using weather API to create Weather App.",
+    link: "https://naomi-clark-weather-app.netlify.app/", //add enter key recognition
+    src: "images/weather.png",
+  },
+  {
+    name: "Clark Ecommerce",
+    date: "12/19/22",
+    description: "Reimagined ecommerce project using Next.js and Mongodb.",
+    link: "https://clark-ecommerce.onrender.com", //slow loading
+    src: "images/ecom.png",
+    collaborator: "Nysa Clark",
+    collablink: "https://github.com/NysaClark",
+  },
+  // {
+  //   name: "Social Hub",
+  //   date: "1/27/23",
+  //   description:
+  //     "CRUD project using PHP and MySQL to create Social Media App in Next.js.",
+  //   link: "http://naomi-phpcrud.infinityfreeapp.com/naomiphpcrud/",
+  //   src: "images/social.png",
+  // },
+];
+
+export default projects;
